ab18978e189e179f3f98e14b1fa8165b402e09a7
Vladko915 <88051272+Vladko915@users.noreply.github.com>
2022-09-11 13:42:57 +0300

    Create wrapper.sh

---
+++ wrapper.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+# Wrapper for create bash-script of Debian
+#Manuals:
+#1) If need to install depencies:
+#  sudo apt-get install -y jq bc pup
+#2) Change and Move this file to /usr/local/bin
+#3) Give permission to run:
+#   chmod +x your_script_name
+
+
+function get_data(){
+
+    echo "first-$1-last";
+}
+
+
+if [ ! -t 0 ] && [ -p /dev/stdin ]; then
+
+    for elem in $(cat /dev/stdin)
+    do
+        result=$(get_data "$elem");
+        echo $result;
+    done
+
+else
+
+    count=1
+    while [ -n "$1" ]
+    do
+        elem=$1
+        if [ "$elem" == "" ];
+        then
+            echo 'error: empty elem';
+        else
+            result=$(get_data "$1");
+            echo $result;
+        fi
+    shift
+    done
+
+fi
